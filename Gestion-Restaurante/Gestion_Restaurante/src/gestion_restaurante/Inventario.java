b4abeea02bb015d7ba177c60fbf67682b4753e0a
Martin <miorgullo926@gmail.com>
2025-05-04 12:13:34 -0500

    Add: manejo de Excepciones

--- Gestion-Restaurante/Gestion_Restaurante/src/gestion_restaurante/Inventario.java
+++ Gestion-Restaurante/Gestion_Restaurante/src/gestion_restaurante/Inventario.java
@@ -4,10 +4,43 @@
  */
 package gestion_restaurante;
 
-/**
- *
- * @author HP
- */
+
 public class Inventario {
+    private Menu menu;
+    private int stock;
+
+    public Inventario(Menu menu, int stock) {
+        this.menu = menu;
+        this.stock = stock;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+
+    public int getStock() {
+        return stock;
+    }
     
+    // incrementa el stock en la cantidad dada
+    public void addStock(int cantidad) {
+        if(cantidad >= 0){
+            this.stock += cantidad;
+        }
+    }
+    // redue el stock si hay suficiente
+    public void reducirStock(int cantidad){
+        if(cantidad <= stock){
+            this.stock -= cantidad;
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return "Nombre = " + menu + ", stock = " + stock;
+    }
 }
