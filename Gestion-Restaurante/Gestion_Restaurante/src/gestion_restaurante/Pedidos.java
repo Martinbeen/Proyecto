d201884092e7144e2afa569b048aa805639476ed
Martin <miorgullo926@gmail.com>
2025-05-04 12:25:46 -0500

    Add: se agregaron 2 metodos: buscarMenu y mostrarInventario

--- Gestion-Restaurante/Gestion_Restaurante/src/gestion_restaurante/Pedidos.java
+++ Gestion-Restaurante/Gestion_Restaurante/src/gestion_restaurante/Pedidos.java
@@ -4,10 +4,55 @@
  */
 package gestion_restaurante;
 
-/**
- *
- * @author HP
- */
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+
 public class Pedidos {
+    private List<Menu> menus = new ArrayList<>();
     
+    // agregar un menu al pedido
+    public void addMenu(Menu menu){
+        menus.add(menu);
+    }
+    // Eliminar un menu del pedido
+    public boolean removeMenu(Menu menu){
+        return menus.remove(menu);
+    }
+    // retorna el total sin impuesto
+    public double getSubtotal(){
+        return menus.stream()
+                .mapToDouble(Menu::getPrecio)
+                .sum();
+    }
+    // Buscar Menu
+    public Menu buscarMenu(String nombre){
+        if(menus == null){
+            return null;
+        }
+        else{
+            for(Menu menu: menus){
+                if(menu.getNombre().equals(nombre)){
+                    return menu;
+                }
+            }
+            return null;
+        }
+    }
+    public void mostrarInventario(){
+        if(this.menus.isEmpty()){
+            JOptionPane.showMessageDialog((Component)null, "Pedidos vacio");
+            return;
+        }
+        else {
+            StringBuilder mensaje = new StringBuilder();
+            mensaje.append("Pedidos: \n");
+            for(Menu menu: menus){
+                mensaje.append(menu.toString());
+            }
+            JOptionPane.showMessageDialog((Component)null, mensaje.toString());
+        }
+    }
 }
